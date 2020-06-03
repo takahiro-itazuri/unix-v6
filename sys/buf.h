b51b5b5da58dfe9c708f497ef8c23deba2f1437f
takahiro-itazuri <takahiro.lab.1226@gmail.com>
2020-06-03 22:33:52 +0900

    add Japanese commentary on buffer cache

--- sys/buf.h
+++ sys/buf.h
@@ -1,77 +1,58 @@
 /*
- * Each buffer in the pool is usually doubly linked into 2 lists:
- * the device with which it is currently associated (always)
- * and also on a list of blocks available for allocation
- * for other use (usually).
- * The latter list is kept in last-used order, and the two
- * lists are doubly linked to make it easy to remove
- * a buffer from one list when it was found by
- * looking through the other.
- * A buffer is on the available list, and is liable
- * to be reassigned to another disk block, if and only
- * if it is not marked BUSY.  When a buffer is busy, the
- * available-list pointers can be used for other purposes.
- * Most drivers use the forward ptr as a link in their I/O
- * active queue.
- * A buffer header contains all the information required
- * to perform I/O.
- * Most of the routines which manipulate these things
- * are in bio.c.
+ * buf 構造体: ブロックデバイスのバッファヘッダ (バッファの管理情報)
+ * 任意のバッファは、2 つの双方向リスト (b_forw/b_back と av_forw/av_back) でリンクされている。
+ * b_forw/b_back は、各ブロックデバイスに割り当てられているバッファを表す。
+ * av_forw/av_back は、処理中 (B_BUSY) でないバッファが、最長不使用 (LRU: Least Recently Used) の順番
+ * で並んでおり、新しいバッファの割り当てに利用される。
+ * 任意のバッファが b-list 内に存在しているが、av-list に存在しているとは限らない。
  */
 struct buf
 {
-	int	b_flags;		/* see defines below */
-	struct	buf *b_forw;		/* headed by devtab of b_dev */
-	struct	buf *b_back;		/*  "  */
-	struct	buf *av_forw;		/* position on free list, */
-	struct	buf *av_back;		/*     if not BUSY*/
-	int	b_dev;			/* major+minor device name */
-	int	b_wcount;		/* transfer count (usu. words) */
-	char	*b_addr;		/* low order core address */
-	char	*b_xmem;		/* high order core address */
-	char	*b_blkno;		/* block # on device */
-	char	b_error;		/* returned after I/O */
-	char	*b_resid;		/* words not transferred after error */
-} buf[NBUF];
+	int	b_flags;		/* フラグ (下部に定義あり) */
+	struct	buf *b_forw;		/* b-list の前方 */
+	struct	buf *b_back;		/* b-list の後方 */
+	struct	buf *av_forw;		/* av-list の前方 */
+	struct	buf *av_back;		/* av-list の後方 */
+	int	b_dev;			/* デバイス番号 (メジャー番号+マイナー番号) */
+	int	b_wcount;		/* 読み書きサイズ */
+	char	*b_addr;		/* メモリ中のバッファデータへのポインタ */
+	char	*b_xmem;		/* b_addr の拡張ビット */
+	char	*b_blkno;		/* ブロック番号 */
+	char	b_error;		/* I/O でエラーが発生した場合に使用する */
+	char	*b_resid;		/* エラーによって転送できなかったデータ量 */
+} buf[NBUF];				/* NBUF は sys/param.h で定義されている */
 
 /*
- * Each block device has a devtab, which contains private state stuff
- * and 2 list heads: the b_forw/b_back list, which is doubly linked
- * and has all the buffers currently associated with that major
- * device; and the d_actf/d_actl list, which is private to the
- * device but in fact is always used for the head and tail
- * of the I/O queue for the device.
- * Various routines in bio.c look at b_forw/b_back
- * (notice they are the same as in the buf structure)
- * but the rest is private to each device driver.
+ * devtab 構造体: 各ブロックデバイスごとに用意される構造体
+ * 2 つの双方向リスト (b_forw/b_back と d_actf/d_actl) を保持している。
+ * b_forw/b_back は、このデバイスのバッファの先頭と最後尾を示している。
+ * d_actf/d_actl は、デバイスの I/O キューの先頭と最後尾を示している。
  */
 struct devtab
 {
-	char	d_active;		/* busy flag */
-	char	d_errcnt;		/* error count (for recovery) */
-	struct	buf *b_forw;		/* first buffer for this dev */
-	struct	buf *b_back;		/* last buffer for this dev */
-	struct	buf *d_actf;		/* head of I/O queue */
-	struct 	buf *d_actl;		/* tail of I/O queue */
+	char	d_active;		/* デバイスの処理中 */
+	char	d_errcnt;		/* エラーカウント */
+	struct	buf *b_forw;		/* バッファの先頭 */
+	struct	buf *b_back;		/* バッファの最後尾 */
+	struct	buf *d_actf;		/* I/O キューの先頭 */
+	struct 	buf *d_actl;		/* I/O キューの最後尾 */
 };
 
-/*
- * This is the head of the queue of available
- * buffers-- all unused except for the 2 list heads.
- */
-struct	buf bfreelist;
+struct	buf bfreelist; /* デバイスに割り当てられていない (NODEV の) バッファの先頭 */
+
 
 /*
- * These flags are kept in b_flags.
+ * バッファのフラグ
+ * buf 構造体の b_flags で使用する。
  */
-#define	B_WRITE	0	/* non-read pseudo-flag */
-#define	B_READ	01	/* read when I/O occurs */
-#define	B_DONE	02	/* transaction finished */
-#define	B_ERROR	04	/* transaction aborted */
-#define	B_BUSY	010	/* not on av_forw/back list */
-#define	B_PHYS	020	/* Physical IO potentially using UNIBUS map */
-#define	B_MAP	040	/* This block has the UNIBUS map allocated */
-#define	B_WANTED 0100	/* issue wakeup when BUSY goes off */
-#define	B_RELOC	0200	/* no longer used */
-#define	B_ASYNC	0400	/* don't wait for I/O completion */
-#define	B_DELWRI 01000	/* don't write till block leaves available list */
+#define	B_WRITE	0	/* 書き出し */
+#define	B_READ	01	/* 読み込み */
+#define	B_DONE	02	/* 最新の情報が保持されている */
+#define	B_ERROR	04	/* エラーが発生した */
+#define	B_BUSY	010	/* バッファは使用中で、av_forw/av_back 上にない */
+#define	B_PHYS	020	/* RAW 入出力中 */
+#define	B_MAP	040	/* UNIBUS マップ (PDP-11/40 では使用しない) */
+#define	B_WANTED 0100	/* バッファは使用中であり、解放されるのを待っているプロセスがある */
+#define	B_RELOC	0200	/* このフラグは使用されていない */
+#define	B_ASYNC	0400	/* 先行読み込み、非同期書き出しを行う */
+#define	B_DELWRI 01000	/* 遅延書き込み */
